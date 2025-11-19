1675b4763db1bd43fdb5dc867e3858d4b0b03336
Shahriar Hussain <husainshahriar@gmail.com>
2025-11-19 18:27:45 +0600

    after some edit

--- solution.ts
+++ solution.ts
@@ -46,21 +46,6 @@ class Person {
     
 }
 
-// interface book{
-//     title: string;
-//     rating: number
-// }
-// function filterByRating(books: book[]): book[] {
-//   return books.filter(book => book.rating >= 4)
-//   .map(book => ({...book, rating: Number(book.rating.toFixed(1))}));
-// }
-// const books: book[] = [
-//   { title: "Book A", rating: 4.5 },
-//   { title: "Book B", rating: 3.2 },
-//   { title: "Book C", rating: 5.0 },
-// ];
-// console.log(filterByRating(books));
-
 interface eachBook {
   title: string;
   rating: number;
