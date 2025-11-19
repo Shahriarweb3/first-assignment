43bd7bca7c6c5a280b3368982d78b86fa485bc2c
Shahriar Hussain <husainshahriar@gmail.com>
2025-11-19 21:20:45 +0600

    commit after finalizing problem 7

--- solution.ts
+++ solution.ts
@@ -88,40 +88,22 @@ function printBookDetails(bookDetails: Book): void{
 }
 
 
-const myBook: Book = {
-  title: 'The Great Gatsby',
-  author: 'F. Scott Fitzgerald',
-  publishedYear: 1925,
-  isAvailable: true,
-};
+function getUniqueValues(arr1: (string | number) [], arr2: (string | number) []): (string | number)[]{
+    const result: (string | number) [] = [];
 
-
-console.log(printBookDetails(myBook));
-
-
-function getUniqueValues(arr1: (string | number)[], arr2: (string | number)[]): (string | number)[] {
-  const result: (string | number)[] = [];
-
-  for (let i = 0; i < arr1.length; i++) {
-    if (!result.includes(arr1[i])) {
-      result.push(arr1[i]);
+    for (let i = 0; i <arr1.length; i++) {
+        if (!result.includes(arr1[i])){
+            result.push(arr1[i]);
+        }
     }
-  }
-
-  for (let i = 0; i < arr2.length; i++) {
-    if (!result.includes(arr2[i])) {
-      result.push(arr2[i]);
+    for (let i = 0; i < arr2.length; i++){
+        if (!result.includes(arr2[i])){
+            result.push(arr2[i]);
+        }
     }
-  }
-
-  return result;
+    return result
+    
 }
-
-const array1 = [1, 2, 3, 4, 5];
-const array2 = [3, 4, 5, 6, 7];
-
-getUniqueValues(array1, array2);
-
 
 interface Product {
   name: string;
